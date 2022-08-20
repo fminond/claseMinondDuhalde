67f668fcd019afc9e9636a7443c2e0fbf584e612
fechemin <fedeminond@gmail.com>
2022-08-20 15:26:38 -0300

    Desafio entregable complementario

--- js/ciclos.js
+++ js/ciclos.js
@@ -1,10 +1,15 @@
 //ejemplo BUCLE for//
 
 
-let metodoPagoCorrecto=("VISA")
+let metodoPagoCorrecto1=("VISA");
+let metodoPagoCorrecto2=("MASTERCARD");
+let metodoPagoCorrecto3=("AMERICAN EXPRESS");
+
 for (let index = 3;index > 0;index--) {
     let metodoPago=prompt("Elegi un metodo de pago para abonar, tenes " + index + " opciones");
-    if (metodoPago==metodoPagoCorrecto) {
+    
+    if (metodoPago==metodoPagoCorrecto1||metodoPago==metodoPagoCorrecto2||metodoPago==metodoPagoCorrecto3) 
+    {
         alert("Barbaro, vas a poder abonar.Te redirigiremos para el pago. Espere unos segundos... ");
         break;
     }
